8c1753428fe94b469bfe8fbe5893cc26b90ea8f1
walther efrain <waltherulloa95@gmail.com>
2021-07-26 23:26:22 -0600

    Primer formulario Inventario

---
+++ Proyecto_Falcom_Bodega/Frm_Inventario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Falcom_Bodega
+{
+    public partial class Frm_Inventario : Form
+    {
+        public Frm_Inventario()
+        {
+            InitializeComponent();
+        }
+    }
+}
